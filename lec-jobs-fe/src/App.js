17baf4f6068bf65806e14f58ca6422b7dd932b4b
SaubhagyaSapkota <saubhagyasapkota444@gmail.com>
2023-07-15 07:36:31 +0545

    edit component

--- lec-jobs-fe/src/App.js
+++ lec-jobs-fe/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Loginn from './components/Loginn';
+import Home from './components/Home';
+//import Loginn from './components/Loginn'; 
 function App() {
   return (
-    <Loginn/>
+    // <Loginn/>
+    <Home />
   );
 }
 
